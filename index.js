63f04962573c872ae164dd1018f4ef2faa786a3f
luigi.perolari@gmail.com <luigi.perolari@gmail.com>
2021-03-28 17:03:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`listening server at ${port}`); });
+app.use(express.static(__dirname + '/public'));
+app.use(express.json({ limit: "10mb" }));
